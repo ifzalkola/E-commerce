16f5c323944b51dd0bc5dd5e5c21842e48fe89ed
unknown <ifzalkola@gmail.com>
2020-05-15 18:26:09 +0530

    Added Redux to the project

--- src/App.js
+++ src/App.js
@@ -4,27 +4,24 @@ import ShopPage from "./pages/shop/shop";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 import "./pages/sign-in-and-sign-up/sign-in-and-sign-up.scss";
 import Header from "./components/header/header";
+import { connect } from "react-redux";
 import { Switch, Route } from "react-router-dom";
+import { setCurrentUser } from "./redux/user/user-actions";
 import { auth, createUserProfileDocument } from "./firebase/firebase";
 import "./App.css";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null,
-    };
-  }
   unsubscribe = null;
   componentWillUnmount() {
     this.unsubscribe();
   }
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribe = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((snapShot) => {
-          this.setState({
+          setCurrentUser({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data(),
@@ -32,7 +29,7 @@ class App extends React.Component {
           });
         });
       } else {
-        this.setState({
+        setCurrentUser({
           currentUser: userAuth,
         });
       }
@@ -41,7 +38,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
           <Route exact path="/shop" component={ShopPage}></Route>
@@ -52,4 +49,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
